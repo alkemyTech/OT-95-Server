baf6c1e4ad45a57bfe9fb8921422728cc6610b8f
Manuel Montañana <mmaanueell@gmail.com>
2021-10-27 17:45:54 -0300

    changed categories migration params

--- app/migrations/20211027025845-create-categories.js
+++ app/migrations/20211027025845-create-categories.js
@@ -9,12 +9,15 @@ module.exports = {
         type: Sequelize.INTEGER,
       },
       name: {
+        allowNull: false,
         type: Sequelize.STRING,
       },
       description: {
+        allowNull: true,
         type: Sequelize.STRING,
       },
       image: {
+        allowNull: true,
         type: Sequelize.STRING,
       },
       createdAt: {
--- app/models/categories.js
+++ app/models/categories.js
@@ -13,9 +13,18 @@ module.exports = (sequelize, DataTypes) => {
   }
   Categories.init(
     {
-      name: { type: DataTypes.STRING, allowNull: false },
-      description: { type: DataTypes.STRING, allowNull: true },
-      image: { type: DataTypes.STRING, allowNull: true },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
     },
     {
       sequelize,

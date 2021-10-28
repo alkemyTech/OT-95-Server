683ab2d303cc07eac40ccd51208dbc07df8cd074
julinuzzo19 <julinuzzo19@gmail.com>
2021-10-28 19:57:27 -0300

    added controller base

---
+++ app/controllers/activityController.js
@@ -0,0 +1,9 @@
+const activityController = {
+  getAll: () => {},
+  getById: (req, res) => {},
+  create: (req, res) => {},
+  update: (req, res) => {},
+  delete: (req, res) => {},
+};
+
+module.exports = activityController;

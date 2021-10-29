b31022205fab85742fe52a626f0ff00acd1280df
Ramiro Boza <ramiro.boza@hotmail.com>
2021-10-28 21:50:59 -0300

    add post route

--- app/routes/users.js
+++ app/routes/users.js
@@ -7,5 +7,6 @@ const usersController = require('../controllers/usersController');
 /* GET users listing. */
 router.get('/', usersController.getAll);
 router.get('/:id', usersController.getOne);
+router.post('/', usersController.createUser);
 
 module.exports = router;

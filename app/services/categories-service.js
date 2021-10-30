737e9091401d66213a4bc0c6e962d4a4c379165f
Manuel Montañana <manuelmontanana57@gmail.com>
2021-10-30 12:02:28 -0300

    refactor categories-repository

--- app/services/categories-service.js
+++ app/services/categories-service.js
@@ -3,7 +3,7 @@ const categoriesRepository = require('../repositories/categories-repository');
 module.exports = {
   getAll: () => categoriesRepository.getCategories(),
   getById: id => categoriesRepository.getCategory(id),
-  create: category => categoriesRepository.create(category),
+  create: category => categoriesRepository.createCategory(category),
   update: (id, category) => categoriesRepository.updateCategory(id, category),
   remove: id => categoriesRepository.deleteCategory(id)
 };

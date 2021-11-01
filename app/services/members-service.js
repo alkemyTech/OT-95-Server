5484cdab0634330f5e511920405cd8894ad48732
Matias Diaz <matidiazruiz42@gmail.com>
2021-11-01 18:59:50 -0300

    fixing exports

--- app/services/members-service.js
+++ app/services/members-service.js
@@ -2,61 +2,51 @@ const membersRepository = require('../repositories/members-repository');
 const status = require('../constants/constants');
 const messages = require('../constants/messages');
 
-const getMemberById = async (id) => {
-  try {
-    const member = await membersRepository.getById(id);
-    if (!member) {
-      return { status: status.NOT_FOUND_ERROR, response: messages.NOT_FOUND_ERROR };
-    }
-    return { status: status.RESPONSE_OK, response: member };
-  } catch (error) {
-    return { status: status.INTERNAL_ERROR, response: messages.INTERNAL_ERROR };
-  }
-};
-
-const getMembers = async () => {
-  try {
-    const members = await membersRepository.getAll();
-    if (!members) {
-      return { status: status.NOT_FOUND_ERROR, response: messages.NOT_FOUND_ERROR };
-    }
-    return { status: status.RESPONSE_OK, response: members };
-  } catch (error) {
-    return { status: status.INTERNAL_ERROR, response: messages.INTERNAL_ERROR };
-  }
-};
-
-const createMember = async (data) => {
-  try {
-    const member = await membersRepository.create(data);
-    return { status: status.RESPONSE_OK_CREATED, response: member };
-  } catch (error) {
-    return { status: status.INTERNAL_ERROR, response: messages.INTERNAL_ERROR };
-  }
-};
-
-const updateMember = async (id, data) => {
-  try {
-    const member = await membersRepository.update(id, data);
-    return { status: status.RESPONSE_OK, response: member };
-  } catch (error) {
-    return { status: status.INTERNAL_ERROR, response: messages.INTERNAL_ERROR };
-  }
-};
-
-const destroyMember = async (id) => {
-  try {
-    const member = await membersRepository.destroy(id);
-    return { status: status.RESPONSE_OK, response: member };
-  } catch (error) {
-    return { status: status.INTERNAL_ERROR, response: messages.INTERNAL_ERROR };
-  }
-};
-
 module.exports = {
-  getMemberById,
-  getMembers,
-  createMember,
-  updateMember,
-  destroyMember
+  getById: async (id) => {
+    try {
+      const member = await membersRepository.getById(id);
+      if (!member) {
+        return { status: status.NOT_FOUND_ERROR, response: messages.NOT_FOUND_ERROR };
+      }
+      return { status: status.RESPONSE_OK, response: member };
+    } catch (error) {
+      return { status: status.INTERNAL_ERROR, response: messages.INTERNAL_ERROR };
+    }
+  },
+  getAll: async () => {
+    try {
+      const members = await membersRepository.getAll();
+      if (members.lenght < 1) {
+        return { status: status.NOT_FOUND_ERROR, response: messages.NOT_FOUND_ERROR };
+      }
+      return { status: status.RESPONSE_OK, response: members };
+    } catch (error) {
+      return { status: status.INTERNAL_ERROR, response: messages.INTERNAL_ERROR };
+    }
+  },
+  create: async (data) => {
+    try {
+      await membersRepository.create(data);
+      return { status: status.RESPONSE_OK_CREATED, response: messages.RESPONSE_OK_CREATED };
+    } catch (error) {
+      return { status: status.INTERNAL_ERROR, response: messages.INTERNAL_ERROR };
+    }
+  },
+  update: async (id, data) => {
+    try {
+      await membersRepository.update(id, data);
+      return { status: status.RESPONSE_OK_UPDATED, response: messages.RESPONSE_OK_UPDATED };
+    } catch (error) {
+      return { status: status.INTERNAL_ERROR, response: messages.INTERNAL_ERROR };
+    }
+  },
+  destroy: async (id) => {
+    try {
+      await membersRepository.destroy(id);
+      return { status: status.RESPONSE_OK, response: messages.RESPONSE_OK };
+    } catch (error) {
+      return { status: status.INTERNAL_ERROR, response: messages.INTERNAL_ERROR };
+    }
+  }
 };

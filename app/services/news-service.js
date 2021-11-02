58404037af2e544a5ba4b71e9fda017664fec9ab
ya1k96 <yamil123_1@live.com>
2021-11-01 22:11:02 -0300

    news services added

---
+++ app/services/news-service.js
@@ -0,0 +1,13 @@
+const newsRepository = require('../repository/news-repository');
+
+const getAll = () => newsRepository.getAll();
+
+const getById = id => newsRepository.getById(id);
+
+const create = (name, image, content) => newsRepository.create(name, image, content);
+
+const update = (name, image, content) => newsRepository.update(name, image, content);
+
+const destroy = id => newsRepository.destroy(id);
+
+module.exports = { getAll, getById, create, update, destroy };

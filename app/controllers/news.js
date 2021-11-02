8dc07fb5cd01cd1b3b351e12479dfb6f0d28de76
ya1k96 <yamil123_1@live.com>
2021-11-01 22:11:39 -0300

    update news controller

--- app/controllers/news.js
+++ app/controllers/news.js
@@ -2,10 +2,16 @@
 
 const codeStatus = require('../constants/constants');
 const messages = require('../constants/messages');
+const newsService = require('../services/news-service');
 
 const getAll = async (req, res) => {
   try {
-    
+    const news = await newsService.getAll();
+    if (news.length > 0) {
+      res.status(codeStatus.RESPONSE_OK).json(news);
+    } else {
+      res.status(codeStatus.RESPONSE_OK_NO_CONTENT).json(messages.RESPONSE_OK_NO_CONTENT);
+    }
   } catch (err) {
     return res.status(codeStatus.BAD_REQUEST_ERROR).json({ message: messages.BAD_REQUEST_ERROR });
   }
@@ -13,7 +19,13 @@ const getAll = async (req, res) => {
 
 const getById = async (req, res) => {
   try {
-    
+    const news = await newssService.getById(req.params.id);
+
+    if (news) {
+      res.status(codeStatus.RESPONSE_OK).json(news);
+    } else {
+      res.status(codeStatus.RESPONSE_OK_NO_CONTENT).json(messages.RESPONSE_OK_NO_CONTENT);
+    }
   } catch (err) {
     return res.status(codeStatus.BAD_REQUEST_ERROR).json({ message: messages.BAD_REQUEST_ERROR });
   }
@@ -21,7 +33,14 @@ const getById = async (req, res) => {
 
 const create = async (req, res) => {
   try {
-    
+    const { name, image, content } = req.body;
+    const newsCreated = await newsService.create(name, image, content);
+
+    if (newsCreated) {
+      res.status(codeStatus.RESPONSE_OK_CREATED).json(messages.RESPONSE_OK_CREATED);
+    } else {
+      res.status(codeStatus.BAD_REQUEST_ERROR).json(messages.BAD_REQUEST_ERROR);
+    }
   } catch (err) {
     return res.status(codeStatus.BAD_REQUEST_ERROR).json({ message: messages.BAD_REQUEST_ERROR });
   }
@@ -29,15 +48,30 @@ const create = async (req, res) => {
 
 const update = async (req, res) => {
   try {
-    
+    const { name, image, content } = req.body;
+    const news = await newssService.getById(req.params.id);
+
+    if (news) {
+      await newssService.update(req.params.id, name, image, content);
+      res.status(codeStatus.RESPONSE_OK).json(messages.RESPONSE_OK);
+    } else {
+      res.status(codeStatus.BAD_REQUEST_ERROR).json(messages.BAD_REQUEST_ERROR);
+    }
   } catch (err) {
     return res.status(codeStatus.BAD_REQUEST_ERROR).json({ message: messages.BAD_REQUEST_ERROR });
   }
 }
 
-const remove = async (req, res) => {
+const destroy = async (req, res) => {
   try {
-    
+    const news = await newssService.getById(req.params.id);
+
+    if (news) {
+      await newssService.destroy(req.params.id);
+      res.status(codeStatus.RESPONSE_OK).json(messages.RESPONSE_OK);
+    } else {
+      res.status(codeStatus.BAD_REQUEST_ERROR).json(messages.BAD_REQUEST_ERROR);
+    }
   } catch (err) {
     return res.status(codeStatus.BAD_REQUEST_ERROR).json({ message: messages.BAD_REQUEST_ERROR });
   }

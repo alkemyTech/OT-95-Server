c8979c5d3e6c1b72ee872cf37535a06bd353b4bb
Ramiro Boza <ramiro.boza@hotmail.com>
2021-11-03 18:23:47 -0300

    added logical of jwt: generation and validation. example in the route api/users

---
+++ app/middlewares/validate-jwt.js
@@ -0,0 +1,24 @@
+const { response, request } = require('express');
+const constants = require('../constants/constants');
+const messages = require('../constants/messages');
+const jwt = require('jsonwebtoken');
+
+const validateJwt = (req = request, res = response, next) => {
+  const token = req.header('x-token');
+  if (!token) {
+    return res.status(constants.BAD_REQUEST_ERROR).json({
+      msg: messages.NO_TOKEN
+    });
+  }
+  try {
+    jwt.verify(token, constants.SECRETORPRIVATEKEY);
+    next();
+  } catch (error) {
+    return res.status(constants.NOK_USER_CREDENTIALS).json({
+      error,
+      msg: messages.INVALID_TOKEN
+    });
+  }
+};
+
+module.exports = validateJwt;

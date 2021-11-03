555ef882170ccc2af13ce94ba85b248981d38856
Matias Diaz <matidiazruiz42@gmail.com>
2021-11-02 23:02:30 -0300

    create routes and middlewares for members

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,11 +4,14 @@ const router = express.Router();
 const usersRouter = require('./users');
 const organizationRouter = require('./organization-route');
 const categoriesRouter = require('./categories-route');
+const membersRouter = require('./members-route');
 
 router.use('/users', usersRouter);
 
 router.use('/organization', organizationRouter);
 
 router.use('/categories', categoriesRouter);
+
+router.use('/members', membersRouter);
 
 module.exports = router;

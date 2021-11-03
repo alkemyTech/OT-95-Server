893b81eaed66942c4acf0b9256341ca1ddae69f4
Manuel Montañana <manuelmontanana57@gmail.com>
2021-11-03 19:18:31 -0300

    fixed name of function getOne

--- app/middlewares/isAdmin.js
+++ app/middlewares/isAdmin.js
@@ -1,11 +1,11 @@
-const { getById } = require('../repositories/users-repository');
+const { getOne } = require('../repositories/users-repository');
 const messages = require('../constants/messages');
 const statusCodes = require('../constants/constants');
 
 module.exports = {
   isAdmin: (req, res, next) => {
     const { id } = req.user;
-    const { roleId } = getById(id);
+    const { roleId } = getOne(id);
 
     if (roleId === 0) {
       next();

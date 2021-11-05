6ab1d81fe767aeb3fc42dd08b5d7392f1f764327
Manuel Montañana <manuelmontanana57@gmail.com>
2021-11-05 18:53:50 -0300

    added token and admin validation to all categories routes

--- app/routes/categories-route.js
+++ app/routes/categories-route.js
@@ -3,12 +3,14 @@ const express = require('express');
 const router = express.Router();
 const categoriesController = require('../controllers/categories-controller');
 const { validateCreate, validateUpdate } = require('../middlewares/categories-middleware');
+const validateJWT = require('../middlewares/validate-jwt');
+const { isAdmin } = require('../middlewares/isAdmin');
 
 // GET all categories by name.
-router.get('/', categoriesController.getAll);
+router.get('/', validateJWT, isAdmin, categoriesController.getAll);
 
-router.post('/', validateCreate, categoriesController.create);
+router.post('/', validateJWT, isAdmin, validateCreate, categoriesController.create);
 
-router.patch('/:id', validateUpdate, categoriesController.update);
+router.patch('/:id', validateJWT, isAdmin, validateUpdate, categoriesController.update);
 
 module.exports = router;

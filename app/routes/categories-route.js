7a6b2387030ea4f5488eefcf46ebbcf1ecd90208
Manuel Montañana <manuelmontanana57@gmail.com>
2021-11-05 18:23:08 -0300

    added endpoint PATCH /categories/:id and middleware

--- app/routes/categories-route.js
+++ app/routes/categories-route.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 const categoriesController = require('../controllers/categories-controller');
-const { validateCreate } = require('../middlewares/categories-middleware');
+const { validateCreate, validateUpdate } = require('../middlewares/categories-middleware');
 
 // GET all categories by name.
 router.get('/', categoriesController.getAll);
 
 router.post('/', validateCreate, categoriesController.create);
+
+router.patch('/:id', validateUpdate, categoriesController.update);
 
 module.exports = router;

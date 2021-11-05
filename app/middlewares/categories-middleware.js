a88bd6c80c4e69896d6255c05f1a50e2bb08ac1e
Manuel Montañana <manuelmontanana57@gmail.com>
2021-11-05 17:57:04 -0300

    changed validateCreate now using the general func validateFields

--- app/middlewares/categories-middleware.js
+++ app/middlewares/categories-middleware.js
@@ -1,20 +1,12 @@
-const { check, validationResult } = require('express-validator');
-const codeStatus = require('../constants/constants');
-const messages = require('../constants/messages');
+const { check } = require('express-validator');
+const validateFields = require('./validate-fields');
+
 
 module.exports = {
-  validateCreate: async (req, res, next) => {
-    await check('name').isString().notEmpty().run(req);
-    await check('description').isString().optional().run(req);
-    await check('image').isString().optional().run(req);
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.status(codeStatus.BAD_REQUEST_ERROR).json({
-        message: messages.BAD_REQUEST_ERROR,
-        errors: errors.array()
-      });
-    }
-    return next();
-  }
+  validateCreate: [
+    check('name').notEmpty().isString(),
+    check('description').optional().isString(),
+    check('image').optional().isString(),
+    validateFields
+  ],
 };

ae4ca385a7ba39ed927dd88dadb180410ba9204a
silvi8794 <silvi8794@gmail.com>
2021-11-04 22:55:51 -0300

    package.json

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const usersRouter = require('./users');
 const organizationRouter = require('./organization-route');
 const categoriesRouter = require('./categories-route');
+const testimoniaslRouter = require('./testimonials-route');
+const activityRouter = require('./activity-routes');
 const authRouter = require('./auth');
 const membersRouter = require('./members-route');
 
@@ -15,6 +17,10 @@ router.use('/auth', authRouter);
 router.use('/organization', organizationRouter);
 
 router.use('/categories', categoriesRouter);
+
+router.use('/testimonials', testimoniaslRouter);
+
+router.use('/activities', activityRouter);
 
 router.use('/members', membersRouter);
 

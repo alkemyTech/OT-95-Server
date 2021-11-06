129d9830e130d8627392823081e5715e2881f1c4
julinuzzo19 <julinuzzo19@gmail.com>
2021-11-06 17:52:08 -0300

    added middleware role for post activity

--- app/routes/activity-routes.js
+++ app/routes/activity-routes.js
@@ -3,7 +3,8 @@ const express = require('express');
 const router = express.Router();
 const { create } = require('../controllers/activity-controller');
 const { validateCreate } = require('../middlewares/activity-middleware');
+const { isAdmin } = require('../middlewares/isAdmin');
 
-router.post('/', validateCreate, create);
+router.post('/', [validateCreate, isAdmin], create);
 
 module.exports = router;

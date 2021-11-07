dda4efd1f29f148716687e664894b27d93fe905e
juanDeGiorgi <juanmadegiorgi@gmail.com>
2021-11-07 18:32:49 -0300

    refactor middleware

--- app/middlewares/ownership-middleware.js
+++ app/middlewares/ownership-middleware.js
@@ -3,7 +3,7 @@ const { isAdmin } = require('./isAdmin');
 module.exports = {
 
   isOwnerShip: (req, res, next) => {
-    if (+req.params.id === req.user.uid) {
+    if (+req.params.id === req.user.id) {
       next();
     } else {
       isAdmin(req, res, next);

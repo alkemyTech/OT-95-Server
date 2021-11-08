8eb7e6721f7d9b773db90f6f74ea68190bb47780
silvi8794 <silvi8794@gmail.com>
2021-11-08 07:38:23 -0300

    Endpoint user Authenticated

---
+++ app/middlewares/decode-token.js
@@ -0,0 +1,25 @@
+const constants = require('../constants/constants');
+const messages = require('../constants/messages');
+const { response, request } = require('express');
+const jwt = require('jsonwebtoken');
+
+const decodeTokn = (req = request, res = response) => {
+  const token = req.header('Authorization');
+  if (!token) {
+    return res.status(constants.BAD_REQUEST_ERROR).json({
+      msg: messages.NO_TOKEN
+    });
+  }
+
+  jwt.verify(token, constants.SECRETORPRIVATEKEY, (err, decoded) => {
+    if (err) {
+      return res.status(constants.NOK_USER_CREDENTIALS).json({
+        err,
+        msg: messages.INVALID_TOKEN
+      });
+    }
+    return res.status(200).send(decoded);
+  });
+};
+
+module.exports = decodeTokn;

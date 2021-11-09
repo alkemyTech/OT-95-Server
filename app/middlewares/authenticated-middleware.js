a81570157075d74b771c10fdd248afa06ce7efa9
silvi8794 <silvi8794@gmail.com>
2021-11-09 19:40:32 -0300

    Delete package-lock

--- app/middlewares/authenticated-middleware.js
+++ app/middlewares/authenticated-middleware.js
@@ -1,11 +1,11 @@
 const messages = require('../constants/messages');
-const codeStatus = require('../constants/constants');
+const statusCode = require('../constants/constants');
 
 module.exports = {
   isAuthenticated: (req, res) => {
     const token = req.header('Authorization');
     if (!token) {
-      res.status(codeStatus.FORBIDDEN).json({ message: messages.FORBIDDEN });
+      res.status(statusCode.FORBIDDEN).json({ message: messages.FORBIDDEN });
     }
   }
 };

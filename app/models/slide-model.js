fdf82c2ec5b0d33ce8b739b605689c6599df0b8f
Matias Diaz <matidiazruiz42@gmail.com>
2021-11-09 18:53:49 -0300

    model and migration for slides

---
+++ app/models/slide-model.js
@@ -0,0 +1,40 @@
+const {
+  Model
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Slide extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate() {
+      // define association here
+    }
+  }
+  Slide.init({
+    imageUrl: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    text: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    order: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    organizationId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'Slide',
+    timestamps: true,
+    paranoid: true
+  });
+  return Slide;
+};

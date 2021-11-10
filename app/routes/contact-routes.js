0693b4adbac407c9d7924a1977f8c3626f74bf64
julinuzzo19 <julinuzzo19@gmail.com>
2021-11-10 14:58:14 -0300

    fixed middleware import in route

--- app/routes/contact-routes.js
+++ app/routes/contact-routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { getAll } = require('../controllers/contact-controller');
-const isAdmin = require('../middlewares/isAdmin');
+const {isAdmin} = require('../middlewares/isAdmin');
 const validateJwt = require('../middlewares/validate-jwt');
 
 router.get('/', [validateJwt, isAdmin], getAll);

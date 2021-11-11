0832b851dc3947f8989765360b486e606f9d9db1
Manuel Montañana <manuelmontanana57@gmail.com>
2021-11-11 20:17:27 -0300

    fixed errors

--- app/config/config.js
+++ app/config/config.js
@@ -24,10 +24,10 @@ module.exports = {
     dialect: 'mysql',
   },
   S3: {
-    accessKeyId: process.env.S3_ACCESS_KEY_ID,
-    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-    region: process.env.S3_REGION,
-    bucket: process.env.S3_BUCKET,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION,
+    bucket: process.env.AWS_BUCKET,
   },
   JWT: {
     secretKey: process.env.JWT_SECRET,

b6db61a8e58d0e35e688f7917bd2fa979cfd982a
Manuel Montañana <manuelmontanana57@gmail.com>
2021-11-11 20:25:44 -0300

    fixed error

--- app/config/config.js
+++ app/config/config.js
@@ -27,7 +27,7 @@ module.exports = {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region: process.env.AWS_DEFAULT_REGION,
-    bucket: process.env.AWS_BUCKET,
+    bucket: process.env.AWS_S3_BUCKET,
   },
   JWT: {
     secretKey: process.env.JWT_SECRET,
